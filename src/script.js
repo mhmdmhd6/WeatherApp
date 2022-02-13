21d0ab198b2c1f404961db0eaa22f94a6799fb06
mhmdmhd6 <mhmdmhdi.al@gmail.com>
2022-02-13 22:44:21 +0330

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 "use strict";
 let cityName = "آمل";
-const audio = new Audio("/sound/rain.mp3");
+const audio = new Audio('rain.mp3', { scope: '../WeatherApp/sound' });
 const body = document.querySelector("body");
 
 function featchWeather() {
