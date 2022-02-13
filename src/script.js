ccac48f62416f63668a9ebe98f8d91d4bf554998
mhmdmhd6 <mhmdmhdi.al@gmail.com>
2022-02-13 21:53:14 +0330

    new features added

--- src/script.js
+++ src/script.js
@@ -1,36 +1,54 @@
-/* Variebel start*/
-const input = document.querySelector('.searchInput');
-const button = document.querySelector('.searchButton');
-const body = document.querySelector('body')
-/* Variebel end*/
-
-/*
-// in function baraye avordan backgrounde bodye
-function main(response) {
-    console.log(response.data);
-    const description = response.data.weather[0].description;
-    console.log(description.split(' ').join('+'));
-    body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/?${description.split(' ').join('+')})`;
-}
-*/
-const cityName = 'آمل';
+"use strict";
+let cityName = "آمل";
+const audio = new Audio("../sound/rain.mp3");
+const body = document.querySelector("body");
 
 function featchWeather() {
-    const apiKey = "60cc000b9167e964953aa85a43e74d51";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric`;
-    axios.get(`${apiUrl}&appid=${apiKey}`).then(displayWeather);
+  const apiKey = "60cc000b9167e964953aa85a43e74d51";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=fa`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(displayWeather);
 }
 function displayWeather(response) {
-    const description = response.data.weather[0].description;
-    body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/?${description.split(' ').join('+')})`;
-    document.querySelector('.cityName').innerHTML = cityName;
+  // const weatherMain = response.data.weather[0].main + " " + "sky";
+  const description = response.data.weather[0].description;
+  const wind = response.data.wind.speed;
+  const humidity = response.data.main.humidity;
+  const temp = response.data.main.temp;
+  const icon = response.data.weather[0].icon;
+  /*
+  body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/?${weatherMain
+    .split(" ")
+    .join("+")})`;
+    */
+  document.querySelector(
+    ".icon"
+  ).src = `https://openweathermap.org/img/wn/${icon}@4x.png`;
+  document.querySelector(".cityName").innerHTML = cityName;
+  document.querySelector(".description").innerHTML = description;
+  document.querySelector(".windSpeed").innerHTML =
+    "سرعت باد : " + wind + " کیلومتر / ساعت";
+  document.querySelector(".temp").innerHTML = temp + "°";
+  document.querySelector(".humidity").innerHTML = "رطوبت : " + humidity + "%";
+  console.log(response.data.weather[0].icon, response.data);
+  if (response.data.weather[0].main == "Rain") {
+    audio.play();
+  }
 }
-
 featchWeather();
+const button = document
+  .querySelector(".searchButton")
+  .addEventListener("click", function click() {
+    cityName = document.querySelector(".searchInput").value;
+    featchWeather();
+  });
+document.querySelector(".searchInput").addEventListener("keyup", function (e) {
+  if (e.key == "Enter") {
+    cityName = document.querySelector(".searchInput").value;
+    featchWeather();
+  }
+});
 
-
-
-                                        /******** previouse code backup *********/
+/******** previouse code backup *********/
 /* 
  Variebel start
 const input = document.querySelector('.searchInput');
